fdc853fc944284c2453c3155306d7fb1749c432a
krupa2029 <kjkotia10@gmail.com>
2022-02-03 16:00:22 +0530

    Feat: Add todo form

--- src/app/MyComponents/todos/todos.component.ts
+++ src/app/MyComponents/todos/todos.component.ts
@@ -33,4 +33,8 @@ export class TodosComponent implements OnInit {
     const index = this.todos.indexOf(todo);
     this.todos.splice(index,1)
   }
+  addTodo(todo:Todo){
+    console.log(todo);
+    this.todos.push(todo)
+  }
 }
